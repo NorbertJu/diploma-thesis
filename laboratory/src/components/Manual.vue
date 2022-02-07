e43c877210059ebd76ee1dbec273119c230e739b
njurik <noro.jurik@gmail.com>
2022-02-07 19:14:23 +0100

    Add manual component

---
+++ laboratory/src/components/Manual.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Manual',
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
--- laboratory/src/components/Menu.vue
+++ laboratory/src/components/Menu.vue
@@ -4,6 +4,7 @@
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
       <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
     </svg>
+    <Manual/>
     <label class="form-label-sm small">Alphabet</label>
     <div @drop="setCurrentlyOver('options')">
       <draggable v-model="options" :sort="false" :group="{ name: 'rules', pull: 'clone', put: false }" @start="onStart">
@@ -128,6 +129,7 @@ import LSystem from '../scripts/L-system';
 import draggable from 'vuedraggable'
 import { mapState } from 'vuex'
 
+import Manual from './Manual'
 import General from './MenuGeneral'
 import Randomness from './MenuRandomness'
 import Structures from './MenuStructures'
@@ -136,13 +138,10 @@ export default {
   name: 'Menu',
   components: {
     draggable,
+    Manual,
     General,
     Randomness,
     Structures
-  },
-  created() {
-    this.$root.$on('getSystem', this.saveSystem)
-    this.$root.$on('loadSystem', this.loadSystem)
   },
   data() {
     return {
