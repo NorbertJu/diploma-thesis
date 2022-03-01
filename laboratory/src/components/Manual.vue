b1c69afc39e8e7d626aba5b3daded59f0e815b0f
njurik <noro.jurik@gmail.com>
2022-03-01 14:33:32 +0100

    Add manual

--- laboratory/src/components/Manual.vue
+++ laboratory/src/components/Manual.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-
+  <div class="info">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="info bi bi-info-circle text-primary" viewBox="0 0 16 16">
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+      <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+    </svg>
+    <div class="manual bg-white rounded shadow">
+      <i>Drag&Drop symbols to build axiom and rules</i></br>
+      </br>
+      <b>Alphabet</b></br>
+      <i>F</i> &rarr; move forward</br>
+      <i>f</i> &rarr; move forward without drawing</br>
+      <i>+</i> &rarr; turn left</br>
+      <i>-</i> &rarr; turn right</br>
+      <i>[ ]</i> &rarr; create branch</br>
+      </br>
+      <b>Axiom</b></br>
+      Initial sequence of symbols</br>
+      </br>
+      <b>Rules</b></br>
+      Rewrite rules specifying the substitution of symbols that generate new symbol sequences</br>
+      </br>
+      <b>Buttons</b></br>
+      <i class="text-primary">Draw</i> &rarr; draw the defined system</br>
+      <i class="text-secondary">Reset</i> &rarr; reset the position of the turtle</br>
+      <i class="text-danger">Clear</i> &rarr; clear the whole canvas</br>
+      </br>
+      <b>Canvas</b></br>
+      <i>Drag&Drop</i> &rarr; change the position of the turtle</br>
+      <i>Drag&Scroll</i> &rarr; change the orientation of the turtle</br>
+      </br>
+    </div>
   </div>
 </template>
 
@@ -13,5 +42,25 @@ export default {
 </script>
 
 <style>
+.info {
+  position: relative;
+  overflow-x: visible;
+}
 
+.info:hover .manual {
+  opacity: 1;
+  transition: opacity 0.5s;
+}
+
+.manual {
+  position: absolute;
+  top: 0;
+  right: 20px;
+  padding: 10px;
+  max-width: 404px;
+  width: max-content;
+  opacity: 0;
+  transition: opacity 0.5s;
+  pointer-events: none;
+}
 </style>
--- laboratory/src/components/Menu.vue
+++ laboratory/src/components/Menu.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="menu bg-light shadow">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="info bi bi-info-circle text-primary" viewBox="0 0 16 16">
-      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-      <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-    </svg>
     <Manual/>
     <label class="form-label-sm small">Alphabet</label>
     <div @drop="setCurrentlyOver('options')">
@@ -442,7 +438,6 @@ export default {
   font-size: .875rem;
   background-color: #fff;
   padding: 0.25rem 0.25rem;
-  position: relative;
 }
 
 .ruleContainer {
@@ -452,7 +447,6 @@ export default {
   font-size: .875rem;
   min-height: calc(1.5em + 0.5rem + 2px);
   background-color: #fff;
-  position: relative;
 }
 
 .ruleKey {
@@ -485,8 +479,6 @@ export default {
 }
 
 .menu {
-  z-index: 0;
-  position: relative;
   float: left;
   width: 444px;
   height: calc(100% - 50px - 1px);
